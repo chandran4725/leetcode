63facb66ba3b9e07dfd5d323683ccfac9765316c
chandran4725 <chandran.g.work@gmail.com>
2025-12-12 11:47:48 +0530

    Time: 0 ms (100%), Space: 10.6 MB (62.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,19 +4,18 @@ public:
         int n = arr.size();
         vector<int> dp(n,0);
         if(n == 1) return arr[0];
-        dp[0] = arr[0];
-        dp[1] = max(arr[0],arr[1]);
+
+        int secPrev = arr[0];
+        int prev = max(arr[0],arr[1]);
 
         for(int i=2;i<n;i++)
         {
-            int left = left = arr[i] + dp[i-2];
-            
-
-            int right = dp[i-1];
-
-            dp[i] = max(left,right);
+            int pick = arr[i] + secPrev;
+            int nonPick = prev;
+            secPrev = prev;
+            prev = max(pick,nonPick);
         }
 
-        return dp[n-1];
+        return prev;
     }
 };
