7e81f8b87c2a4cf05bcfb4e5ea9663055f0b78e9
chandran4725 <chandran.g.work@gmail.com>
2025-12-13 16:17:56 +0530

    Time: 0 ms (100%), Space: 8.9 MB (31.86%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-
-    void findCombinations(int ind,vector<int> &arr,vector<int> &ds,int k,int target,vector<vector<int>> &ans)
+    void findCombinations(int ind,int k,int n,vector<int> &ds,vector<vector<int>> &ans)
     {
-        if(target < 0) return;
+        
 
-        if(ds.size() == k && target == 0)
+        if(k == 0 && n == 0)
         {
             ans.push_back(ds);
             return;
         }
 
-        if(ind == arr.size()) return;
-        
-        ds.push_back(arr[ind]);
-        findCombinations(ind+1,arr,ds,k,target-arr[ind],ans);
-        ds.pop_back();
-        
+        if(k <= 0 || n < 0 || ind >= 9) return;
 
-        findCombinations(ind+1,arr,ds,k,target,ans);
+        int el = ind+1;
+        ds.push_back(el);
+        findCombinations(ind+1,k-1,n-el,ds,ans);
+        ds.pop_back();
+        findCombinations(ind+1,k,n,ds,ans);
     }
+  
 
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> arr = {1,2,3,4,5,6,7,8,9};
-        vector<vector<int>> ans;
-        vector<int> ds;
+       vector<vector<int>> ans;
+       vector<int> ds;
 
-        findCombinations(0,arr,ds,k,n,ans);
+       findCombinations(0,k,n,ds,ans);
 
-        return ans;
+       return ans;
     }
 };
