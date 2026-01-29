ffc2e3fbd5b363af88eefa6da01c5447cbc171c0
chandran4725 <chandran.g.work@gmail.com>
2026-01-30 00:14:31 +0530

    Time: 2 ms (4.78%), Space: 10.9 MB (32.79%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -22,7 +22,28 @@ public:
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        traverse(root,ans);
+
+        if(root == NULL) return ans;
+
+        stack<TreeNode*> st;
+
+        TreeNode* node = root;
+        while(true)
+        {
+            if(node != NULL)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if(st.empty() == true) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
         return ans;
     }
 };
