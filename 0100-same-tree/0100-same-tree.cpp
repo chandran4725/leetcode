4c65dc149525635501c010bae635e2c7f4d6182d
chandran4725 <chandran.g.work@gmail.com>
2026-02-02 20:57:02 +0530

    Time: 0 ms (100%), Space: 12.8 MB (46.38%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,37 +11,8 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* node,vector<int>& arr)
-    {
-        if(node == NULL) 
-        {
-            arr.push_back(INT_MIN);
-            return;
-        }
-
-        arr.push_back(node->val);
-        traverse(node->left,arr);
-        traverse(node->right,arr);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int> arr1;
-        vector<int> arr2;
-
-        traverse(p,arr1);
-        traverse(q,arr2);
-
-        int ind = 0;
-        int n = arr1.size();
-        int m = arr2.size();
-
-        if(n == 0 || m == 0) return false;
-
-        while(ind < n && ind < m)
-        {
-            if(arr1[ind] != arr2[ind]) return false;
-            ind++;
-        }
-
-        return true;
+        if(p == NULL || q == NULL) return (p==q);
+        return (p->val == q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
